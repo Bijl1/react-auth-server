5a54340f6249ad183e16ca4d05390458e588a17c
Jonathan Bijl <sannoayo@gmail.com>
2023-07-30 22:20:42 -0400

    Token Verification route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User.model");
  
 const router = express.Router();
+const { isAuthenticated } = require("./../middleware/jwt.middleware.js");
 const saltRounds = 10;
 
 // POST /auth/signup  - Creates a new user in the database
@@ -116,8 +117,17 @@ router.post('/login', (req, res, next) => {
   });
   
   
-  // GET  /auth/verify
-  // ...
+// GET  /auth/verify  -  Used to verify JWT stored on the client
+router.get('/verify', isAuthenticated, (req, res, next) => {       // <== CREATE NEW ROUTE
+ 
+    // If JWT token is valid the payload gets decoded by the
+    // isAuthenticated middleware and made available on `req.payload`
+    console.log(`req.payload`, req.payload);
+   
+    // Send back the object with user data
+    // previously set as the token payload
+    res.status(200).json(req.payload);
+  });
   
   module.exports = router;
   
